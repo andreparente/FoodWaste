255f100a4c1e7bdc518f9faa631c85da287a47e6
andreparente <andremachadoparente@gmail.com>
2016-08-12 01:05:46 -0300

    mudancinhas no login

--- FoodWaste/LoginViewController.swift
+++ FoodWaste/LoginViewController.swift
@@ -11,7 +11,7 @@ import TextFieldEffects
 
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTxtField: HoshiTextField!
     
     @IBOutlet weak var passwordTextField: HoshiTextField!
@@ -21,15 +21,47 @@ class LoginViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
     @IBAction func signUpAction(sender: UIButton) {
         performSegueWithIdentifier("LoginToRegisterViewController", sender: self)
+    }
+    
+    @IBAction func loginAction(sender: UIButton) {
+        
+
+        if emailTxtField.text == "" || passwordTextField.text == "" {
+            
+            //alert dizendo que é preciso preencher todos os campos
+               let alert = UIAlertController(title: "Warning", message: "You need to enter with email and password", preferredStyle: UIAlertControllerStyle.Alert)
+        let alertAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+
+        alert.addAction(alertAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            
+            if self.validateEmail(emailTxtField.text!) {
+                //fazer login verificando a senha
+                
+                // performSegueWithIdentifier("LoginToMainViewController", sender: self)
+            } else {
+                let alert = UIAlertController(title: "Warning", message: "your email is not correct", preferredStyle: UIAlertControllerStyle.Alert)
+                let alertAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+                
+                alert.addAction(alertAction)
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func validateEmail(candidate: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluateWithObject(candidate)
     }
     
     override func viewWillAppear(animated: Bool) {
