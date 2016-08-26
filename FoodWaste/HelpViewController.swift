8f2ff082798f4d5b495b299fc3bb808883f8063a
Felipe Viberti <felipegus8@yahoo.com.br>
2016-08-26 13:54:40 -0300

    Tela de help

--- FoodWaste/HelpViewController.swift
+++ FoodWaste/HelpViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class HelpViewController: UIViewController {
-
+class HelpViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    var index:Int!
+    
+    @IBOutlet weak var table: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        table.delegate = self
+        table.dataSource = self
         // Do any additional setup after loading the view.
     }
 
@@ -20,7 +23,31 @@ class HelpViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func back(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath.row)
+        index = indexPath.row
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.table.dequeueReusableCellWithIdentifier("cell2") as! HelpCell
+        switch indexPath.row
+        {
+        case 0:
+            cell.titleLabel.text = "How can I win points?"
+        case 1:
+            cell.titleLabel.text = "How much my donation is worth?"
+        default:
+            break
+        }
+        return cell
+    }
+
 
     /*
     // MARK: - Navigation
@@ -32,4 +59,9 @@ class HelpViewController: UIViewController {
     }
     */
 
+}
+
+public class HelpCell:UITableViewCell
+{
+    @IBOutlet weak var titleLabel:UILabel!
 }
